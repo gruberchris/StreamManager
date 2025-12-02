b3b6cd540f952d2c47ff03e0946c75b611c7cde2
Chris Gruber <gruber.christopher@gmail.com>
2025-12-01 23:33:18 -0500

    initial commit

---
+++ StreamManager.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StreamManager.Web.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
